3861641f6c056149b9532610c72a571c04db7204
michael.jeremias <michael.jeremias.home@gmail.com>
2022-05-03 09:02:47 +0200

    Update sums.py

--- sums.py
+++ sums.py
@@ -1,16 +1,16 @@
 from typing import List
 
 
-class Solution:
-    # Brute force
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sums = {
-            sum([nums[i], nums[j]]): [i, j]
-            for i in range(len(nums) - 1, -1, -1)
-            for j in range(len(nums) - 1, -1, -1)
-            if i < j
-        }
-        return sums[target]
+# class Solution:
+#     # Brute force
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         sums = {
+#             sum([nums[i], nums[j]]): [i, j]
+#             for i in range(len(nums) - 1, -1, -1)
+#             for j in range(len(nums) - 1, -1, -1)
+#             if i < j
+#         }
+#         return sums[target]
 
 
 class Solution:
