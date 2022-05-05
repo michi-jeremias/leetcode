187768fd9ca3bfa55b6163c95b3b2f6306a8bfa6
michael.jeremias <michael.jeremias.home@gmail.com>
2022-05-05 12:10:40 +0200

    tribonacci

--- dynamic_programming/nth_tribonacci_number.py
+++ dynamic_programming/nth_tribonacci_number.py
@@ -1,14 +1,46 @@
+"""
+https://leetcode.com/problems/n-th-tribonacci-number
+
+Problem 2 in
+https://leetcode.com/study-plan/dynamic-programming/
+"""
+
+
 class Solution:
+    # bottom up iterative solution
     def __init__(self) -> None:
-        hash = dict()
-        hash[0] = 0
-        hash[1] = 1
-        hash[2] = 1
+        self.hash = dict()
+        self.hash[0] = 0
+        self.hash[1] = 1
+        self.hash[2] = 1
 
     def tribonacci(self, n: int) -> int:
 
         if n not in self.hash:
             for elem in range(3, n + 1):
-                hash[elem] = hash[elem - 1] + hash[elem - 2] + hash[elem - 3]
+                self.hash[elem] = (
+                    self.hash[elem - 1] + self.hash[elem - 2] + self.hash[elem - 3]
+                )
 
-        return hash[n]
+        return self.hash[n]
+
+
+class Solution:
+    # top down recursive solution
+    def __init__(self) -> None:
+        self.hash = dict()
+        self.hash[0] = 0
+        self.hash[1] = 1
+        self.hash[2] = 1
+
+    def tribonacci(self, n: int) -> int:
+
+        if n not in self.hash:
+            self.tribonacci(n - 1)
+            self.hash[n] = self.hash[n - 1] + self.hash[n - 2] + self.hash[n - 3]
+
+        return self.hash[n]
+
+
+sol = Solution()
+sol.tribonacci(4)
--- tests/test_nth_tribonacci_number.py
+++ tests/test_nth_tribonacci_number.py
@@ -6,4 +6,4 @@ from dynamic_programming.nth_tribonacci_number import Solution
 class TestSolution(unittest.TestCase):
     def test_tribonacci(self):
         sol = Solution()
-        self.assertEqual(sol.trib(), something)
+        self.assertEqual(sol.tribonacci(25), 1389537)
