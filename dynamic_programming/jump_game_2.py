f326d8aee93bcb207bfa11fcf7342e591990781c
michael.jeremias <michael.jeremias.home@gmail.com>
2022-05-12 15:22:10 +0200

    Update jump_game_2.py

--- dynamic_programming/jump_game_2.py
+++ dynamic_programming/jump_game_2.py
@@ -12,27 +12,38 @@ from typing import List
 
 class Solution:
     def __init__(self) -> None:
+        self.min_jumps_needed = defaultdict(int)
         self.hash = defaultdict(int)
 
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
 
-        if n <= 2:
-            return 1
+        self.min_jumps_needed[0] = 0
+        for i in range(1, n):
+            self.min_jumps_needed[i] = 1e5
 
-        self.hash[0] = 1
-        self.hash[1] = 1
+        current_jumps = 0
+        current_min_jumps = 0
 
-        current_jumps = 1
-        for i in range(2, n, 1):
-
-            for j in range(0, i, 1):
-                if nums[j] >= i - j:
-                    pass
-
-            pass
+        for current_position in range(0, n, 1):
+            current_jumps += 1
+            for step in range(1, nums[current_position] + 1, 1):
+                if current_position + step <= n - 1:
+                    current_min_jumps = self.min_jumps_needed[current_position + step]
+                    self.min_jumps_needed[current_position + step] = min(
+                        current_jumps, self.min_jumps_needed[current_position + step]
+                    )
+            print(self.min_jumps_needed)
+            current_jumps = min(current_jumps, current_min_jumps)
+        return self.min_jumps_needed[n - 1]
 
 
 sol = Solution()
-sol.jump([2, 3, 1, 1, 4])
-nums = [2, 3, 1, 1, 4]  # 2
+sol.jump([1, 2, 1, 1, 1])  # 3
+
+sol = Solution()
+sol.jump([2, 3, 1, 1, 4])  # 2
+
+sol = Solution()
+sol.jump([9, 7, 9, 4, 8, 1, 6, 1, 5, 6, 2, 1, 7, 9, 0])  # 2
+
