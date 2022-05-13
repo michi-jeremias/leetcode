34f2700ae5c4b95c8d810750e4dc582c2cb3258e
michael.jeremias <michael.jeremias.home@gmail.com>
2022-05-13 14:51:39 +0200

    Update maximum_subarray.py

--- dynamic_programming/maximum_subarray.py
+++ dynamic_programming/maximum_subarray.py
@@ -10,18 +10,51 @@ from collections import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        pass
+        return sum(self.reduceList(nums))
 
-    def find_max_interval(self, nums) -> List[int]:
+    def reduceListLeft(self, nums):
+        newnums = nums
+        if len(nums) > 2:
+            if sum([nums[0], nums[1]]) < 0 and nums[1] < nums[0]:
+                newnums = self.reduceList(nums[2:])
 
-        if len(nums) > 1:
-            start = 0
-            mid = len(nums) // 2
-            end = len(nums) - 1
-
-            self.find_max_interval(nums[start : mid + 1])
-            self.find_max_interval(nums[mid : end + 1])
-
-        else:
+            if nums[0] + nums[1] < nums[1] or nums[0] < 0:
+                newnums = self.reduceList(nums[1:])
+        if len(nums) == 2:
+            if nums[0] + nums[1] < nums[0] or nums[0] + nums[1] < nums[1]:
+                return [max(nums)]
             return nums
 
+        return newnums
+
+    def reduceListRight(self, nums):
+        newnums = nums
+        if len(nums) > 2:
+            if sum([nums[-1], nums[-2]]) < 0 and nums[-2] < nums[-1]:
+                newnums = self.reduceList(nums[:-2])
+
+            if nums[-1] + nums[-2] < nums[-2] or nums[-1] < 0:
+                newnums = self.reduceList(nums[:-1])
+        if len(nums) == 2:
+            if nums[0] + nums[1] < nums[0] or nums[0] + nums[1] < nums[1]:
+                return [max(nums)]
+            return nums
+        return newnums
+
+    def reduceList(self, nums):
+        nums = self.reduceListLeft(nums)
+        nums = self.reduceListRight(nums)
+        return nums
+
+
+sol = Solution()
+sol.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])  # 6
+
+sol = Solution()
+sol.maxSubArray([-1, -2])  # -1
+
+sol = Solution()
+sol.maxSubArray([1, 2])  # 3
+
+sol = Solution()
+sol.maxSubArray([31, -41, 59, 26, -53, 58, 97, -93, -23, 84])  # 187
